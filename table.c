deb07bbd9f0d75d46bde70b2a997e454ce2084d9
M-Danish-Meraj <muhammaddanishmeraj@gmail.com>
2024-10-15 10:00:53 +0500

    Add files via upload

---
+++ table.c
@@ -0,0 +1,32 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,i ;
+    //printf("enter a num:") ;
+    //scanf("%d",&a);
+ for ( i = 1; i <=10; i++)
+ {
+     printf ("%d\n",i);
+ } 
+    return 0;
+} 
+
+/*
+#include <stdio.h>
+
+int main()
+{
+    int i=0;
+
+    do
+    {
+     printf("%d hello\n",i) ;
+     i++ ;
+    
+    } while (i<=10);
+    
+    return 0;
+}
+*/
